0f5965128b9a46e14efc68196f599db7726c3b97
lucas-hirata <lucashirata.dev@gmail.com>
2021-04-24 19:41:37 -0300

    implementado carregamento de fonte, svg e imagens, adicionado assets

--- App.js
+++ App.js
@@ -1,3 +1,33 @@
-import Index from './src';
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 
-export default Index;
+import Index from './src/index';
+
+export default class App extends React.Component {
+    state = {
+        isReady: false,
+    };
+
+    render() {
+        if (!this.state.isReady) {
+            return (
+                <AppLoading
+                    startAsync={this._cacheResourcesAsync}
+                    onFinish={() => this.setState({ isReady: true })}
+                    onError={console.warn}
+                />
+            );
+        }
+
+        return <Index />;
+    }
+
+    async _cacheResourcesAsync() {
+        return Font.loadAsync({
+            'roboto-light': require('./src/assets/fonts/Roboto-Light.ttf'),
+            'roboto-regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
+            'roboto-bold': require('./src/assets/fonts/Roboto-Bold.ttf'),
+        });
+    }
+}
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        width: Dimensions.get('window').width,
     },
 });
